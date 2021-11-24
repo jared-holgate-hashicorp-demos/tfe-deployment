4c72641e1a8b528669326c0e7835e170ab619f82
Jared Holgate <jared.holgate@hashicorp.com>
2021-11-24 11:50:01 +0000

    Add AWS VPC

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
