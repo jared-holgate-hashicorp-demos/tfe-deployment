8808fbc1ff054ea22190c0042ee0297106af4acb
Jared Holgate <jared.holgate@hashicorp.com>
2021-11-24 15:57:47 +0000

    Add some data sources

--- main.tf
+++ main.tf
@@ -170,3 +170,21 @@ module "vpc_endpoints" {
     }
   }
 }
+
+data "aws_region" "current" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
