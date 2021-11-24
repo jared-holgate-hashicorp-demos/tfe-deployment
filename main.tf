4eae588362b5b07e8bbb710b2726d2fdb91da610
Jared Holgate <jared.holgate@hashicorp.com>
2021-11-24 12:30:33 +0000

    USe the VPC module

--- main.tf
+++ main.tf
@@ -1,5 +1,26 @@
-variable "vpc_name" {
-    default = "tfe-deployment-aws-standalone-external-services"
+variable "friendly_name_prefix" {
+  type        = string
+  description = "(Optional) Friendly name prefix used for tagging and naming AWS resources."
+  default = "tfe-deployment-aws-standalone-external-services"
+}
+
+# Network
+variable "network_cidr" {
+  type        = string
+  description = "(Optional) CIDR block for VPC."
+  default     = "10.0.0.0/16"
+}
+
+variable "network_private_subnet_cidrs" {
+  type        = list(string)
+  description = "(Optional) List of private subnet CIDR ranges to create in VPC."
+  default     = ["10.0.32.0/20", "10.0.48.0/20"]
+}
+
+variable "network_public_subnet_cidrs" {
+  type        = list(string)
+  description = "(Optional) List of public subnet CIDR ranges to create in VPC."
+  default     = ["10.0.0.0/20", "10.0.16.0/20"]
 }
 
 terraform {
@@ -22,10 +43,48 @@ provider "aws" {
   region = "eu-west-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 3.0"
 
-  tags = {
-    Name = var.vpc_name
+  azs                            = data.aws_availability_zones.available.names
+  cidr                           = var.network_cidr
+  create_igw                     = true
+  default_security_group_egress  = []
+  default_security_group_ingress = []
+  enable_dns_hostnames           = true
+  enable_dns_support             = true
+  enable_nat_gateway             = true
+  manage_default_security_group  = true
+  map_public_ip_on_launch        = true
+  name                           = "${var.friendly_name_prefix}-tfe-vpc"
+  one_nat_gateway_per_az         = false
+  private_subnets                = var.network_private_subnet_cidrs
+  public_subnets                 = var.network_public_subnet_cidrs
+  single_nat_gateway             = false
+
+  igw_tags = {
+    Name = "${var.friendly_name_prefix}-tfe-igw"
+  }
+  nat_eip_tags = {
+    Name = "${var.friendly_name_prefix}-tfe-nat-eip"
+  }
+  nat_gateway_tags = {
+    Name = "${var.friendly_name_prefix}-tfe-tgw"
+  }
+  private_route_table_tags = {
+    Name = "${var.friendly_name_prefix}-tfe-rtb-private"
+  }
+  private_subnet_tags = {
+    Name = "${var.friendly_name_prefix}-private"
+  }
+  public_route_table_tags = {
+    Name = "${var.friendly_name_prefix}-tfe-rtb-public"
+  }
+  public_subnet_tags = {
+    Name = "${var.friendly_name_prefix}-public"
+  }
+  vpc_tags = {
+    Name = "${var.friendly_name_prefix}-tfe-vpc"
   }
 }
