0cf30c330346da55f13798b428e0910b2ed74e7b
jaredfholgate <jared.holgate@hashicorp.com>
2022-01-26 11:52:13 +0000

    Add route table

--- main.tf
+++ main.tf
@@ -53,6 +53,19 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.example.id
+
+  route {
+    cidr_block = "10.0.1.0/24"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.friendly_name_prefix}-route-table"
+  }
+}
+
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
