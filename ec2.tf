13d6d5b1d28dccbab9e21393fdd20e4ed8f857e4
jaredfholgate <jared.holgate@hashicorp.com>
2022-01-27 15:23:47 +0000

    Try changing to no tty

--- ec2.tf
+++ ec2.tf
@@ -83,7 +83,7 @@ EOF
 #!/bin/bash
 apt update -y
 apt install ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --no-tty --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
