3682d39e2d9e38820429ba2952ce7b1a692dce85
jaredfholgate <jared.holgate@hashicorp.com>
2022-01-27 13:46:04 +0000

    Fix typo

--- ec2.tf
+++ ec2.tf
@@ -73,7 +73,7 @@ locals {
   hello_word_script = <<EOF
 #!/bin/bash
 apt update -y
-apt install apache2 -yclear
+apt install apache2 -y
 systemctl start apache2.service
 cd /var/www/html
 echo "<html><body><h1>Hello World - Server %s</h1></body></html>" > index.html 
