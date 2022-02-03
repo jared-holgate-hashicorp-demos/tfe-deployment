b8ab2b6ed4ca7a96af02eabf068fedd4697cdbec
jaredfholgate <jared.holgate@hashicorp.com>
2022-02-03 16:03:25 +0000

    Add outputs for manual install mode

--- outputs.tf
+++ outputs.tf
@@ -6,11 +6,26 @@ output "tfe_password" {
   value = nonsensitive(random_password.tfe.result)
 }
 
+output "rds_endpoint" {
+  value = aws_db_instance.postgresql.endpoint
+}
+
 output "rds_username" {
   value = nonsensitive(random_password.rds_username.result)
 }
 
-
 output "rds_password" {
   value = nonsensitive(random_password.rds_password.result)
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.tfe_data_bucket.bucket_domain_name
+}
+
+output "s3_bucket_region" {
+  value = aws_s3_bucket.tfe_data_bucket.region
+}
+
+output "s3_bucket_key" {
+  value = aws_kms_key.data.arn
 }
