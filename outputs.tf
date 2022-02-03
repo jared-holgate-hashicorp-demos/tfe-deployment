76ecfa4f6acc3202ea7afa066fd0a22ad247547d
jaredfholgate <jared.holgate@hashicorp.com>
2022-02-03 15:54:08 +0000

    Output db credentials for manual mode

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,12 @@ output "replicated_password" {
 output "tfe_password" {
   value = nonsensitive(random_password.tfe.result)
 }
+
+output "rds_username" {
+  value = nonsensitive(random_password.rds_username.result)
+}
+
+
+output "rds_password" {
+  value = nonsensitive(random_password.rds_password.result)
+}
